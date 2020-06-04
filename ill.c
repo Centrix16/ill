0f7b98be31f39f825dcd1fbb3c1c273d10e88f28
Centrix14 <artiom.zvyagin@yandex.ru>
2020-06-04 17:14:13 +0300

    add full freeimage and lode-ng support

--- ill.c
+++ ill.c
@@ -5,6 +5,7 @@
 #include "ill.h"
 
 ILL_IMAGE data_image;
+static char lib = LIB_LODEPNG;
 
 ILL_IMAGE illLoadImage(int lib_type, char *image_name) {
 	FIBITMAP *image = NULL;
@@ -17,7 +18,9 @@ ILL_IMAGE illLoadImage(int lib_type, char *image_name) {
 			err = lodepng_decode32_file(&data_image.data, &data_image.width, &data_image.height, image_name);	
 			if (err)
 				data_image.data = NULL;
+			lib = LIB_LODEPNG;
 		break;
+
 		case LIB_FREE_IMAGE:
 			fif = FreeImage_GetFileType(image_name, 0);
 			image = FreeImage_Load(fif, image_name, 0);
@@ -26,18 +29,28 @@ ILL_IMAGE illLoadImage(int lib_type, char *image_name) {
 			data_image.width = FreeImage_GetWidth(image);
 			data_image.height = FreeImage_GetHeight(image);
 
-			FreeImage_Unload(image);
-
+			lib = LIB_FREE_IMAGE;
 		break;
+
 		default:
 			data_image.data = NULL;
 			data_image.width = 0;
 			data_image.height = 0;
+
+			lib = LIB_NIL;
 		break;
 	}
 	return data_image;
 }
 
 void illEnd(ILL_IMAGE *img) {
-	free(img);
+	switch (lib) {	
+		case LIB_LODEPNG:
+			free(img);
+		break;			
+
+		case LIB_FREE_IMAGE:
+			FreeImage_Unload(image);
+		break;	
+	}
 } 
